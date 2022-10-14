a747ebd52059604daaaf0af48cf6d5d61cf6cd44
Rei <umbrarei@gmail.com>
2022-10-14 14:25:33 -0400

    Added AccountTypeRepository

---
+++ BankingApp/AccountTypes/AccountTypeRepository.cs
@@ -0,0 +1,17 @@
+﻿using BankingApp.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankingApp.AccountTypes
+{
+    public class AccountTypeRepository : GenericRepository<AccountType>
+    {
+        public AccountTypeRepository(BankContext context)
+            : base(context)
+        {
+
+        }
+    }
+}
