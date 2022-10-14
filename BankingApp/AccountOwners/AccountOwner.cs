d5ba0b0823acd3d62eec77af16db7b7fa0c6f845
Rei <umbrarei@gmail.com>
2022-10-14 18:01:17 -0400

    Added Account and AccountOwner models

---
+++ BankingApp/AccountOwners/AccountOwner.cs
@@ -0,0 +1,21 @@
+﻿using BankingApp.Accounts;
+using BankingApp.Users;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankingApp.AccountOwners
+{
+    public class AccountOwner
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+
+        public User User { get; set; }
+        public Account Account { get; set; }
+    }
+}
---
+++ BankingApp/Accounts/Account.cs
@@ -0,0 +1,28 @@
+﻿using BankingApp.AccountOwners;
+using BankingApp.AccountTypes;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BankingApp.Accounts
+{
+    public class Account
+    {
+        [Key]
+        [DatabaseGenerated(DatabaseGeneratedOption.Identity)]
+        public int ID { get; set; }
+        public AccountType AccountType { get; set; }
+        public float Balance { get; set; }
+        public string Nickname { get; set; }
+
+        [DefaultValue(true)]
+        public bool IsActive { get; set; }
+        
+
+        public ICollection<AccountOwner> Owners { get; set; }
+    }
+}
