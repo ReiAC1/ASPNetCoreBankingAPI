38d203859978995639d39c8ef792408ade31481b
Rei <umbrarei@gmail.com>
2022-10-18 14:08:40 -0400

    Added update and delete methods

--- BankingApp/Accounts/AccountController.cs
+++ BankingApp/Accounts/AccountController.cs
@@ -66,5 +66,45 @@ namespace BankingApp.Accounts
 
             return _aService.Persist(a) != null;
         }
+
+        [HttpPut("update")]
+        public AccountResponse Update([FromBody] AccountRequest request)
+        {
+            User authUser = _userService.GetUserById(AuthUserID);
+
+            // return false if we have no authentication
+            if (authUser == null)
+                return null;
+
+            Account account = _aService.GetAccountById(request.ID);
+
+            // return false if there is no account, or if the authorized user's id does not match an owner's ID
+            if (account == null || account.Owners.Select(a => a.User.ID == authUser.ID).ToArray().Length == 0)
+                return null;
+
+            // return the updated object
+            return new AccountResponse(_aService.SaveOrUpdate(request));
+        }
+
+        [HttpDelete("deactivate/{id}")]
+        public bool Deactivate(int id)
+        {
+            User authUser = _userService.GetUserById(AuthUserID);
+
+            // return false if we have no authentication
+            if (authUser == null)
+                return false;
+
+            Account account = _aService.GetAccountById(id);
+
+            // return false if there is no account, or if the authorized user's id does not match an owner's ID
+            // additionally the balance must be less than a dollar to close
+            if (account == null || account.Owners.Select(a => a.User.ID == authUser.ID).ToArray().Length == 0 ||
+                Math.Abs(account.Balance) < 1)
+                return false;
+
+            // return whether or not the delete method works
+            return _userService.Delete(id) == null;
+        }
     }
 }
